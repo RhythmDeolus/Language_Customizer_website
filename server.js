f2f09df1efdf24061ad281ab1757740124c297d7
Rhythm Deolus <122043902+RhythmDeolus@users.noreply.github.com>
2023-12-23 18:58:06 +0530

    fdshaklf

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ app.use('/dist', express.static(path.join(__dirname, 'dist')));
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
-})
+});
 
 app.listen(process.env.PORT || 8080
     , function (err) {
         if (err) console.error("Error while starting the server.\n", err);
-        console.log("Runing the server on: ", `http://127.0.0.1:${process.env.PORT || 8080}/creator`);
-    })
+        console.log("Runing the server on: ", `http://127.0.0.1:${process.env.PORT || 8080}/`);
+});
